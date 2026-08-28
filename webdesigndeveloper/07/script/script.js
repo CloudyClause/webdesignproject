4eae0ba08219dfb5c16b902a1c168591d5e3e8ae
CloudyClause <bruanjung@gmail.com>
2026-08-28 19:55:27 +0900

    Add files via upload

--- webdesigndeveloper/07/script/script.js
+++ webdesigndeveloper/07/script/script.js
@@ -1,3 +1,20 @@
 $(function(){
-    
+    $(".gnb").mouseenter(function(){
+        $(".depth2,.gnbbg").slideDown()
+    })
+    $(".gnb").mouseleave(function(){
+        $(".depth2,.gnbbg").slideUp()
+    })
+
+    // family 버튼을 한번 보이고/숨김
+    $("button").click(function(){
+        $(".list").fadeToggle();
+    })
+
+
+
+
+
 })
+
+
